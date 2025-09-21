e17070d73476a1bcf35f52e77c93035212abc8d6
AYUSH <94125343+AYUSH-71643@users.noreply.github.com>
2025-09-21 23:10:13 +0530

    Update C2.c

--- c/C2.c
+++ c/C2.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main() {
 int a, b;
-printf("Write value of A and press enter");
-scanf("%d",&a)
-printf("Write value of B and press enter");
-scanf("%d",&b)
-
+printf("Write value of A and press enter: ");
+scanf("%d",&a);
+printf("\n");
+printf("Write value of B and press enter: ");
+scanf("%d",&b);
+printf("\n");
 printf("%d\n",a+b);
 return 0;
 }
