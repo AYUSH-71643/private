d1f741ac5f8ac3f13eb7f18b4238fc4c0d36d4a2
AYUSH <94125343+AYUSH-71643@users.noreply.github.com>
2025-09-21 23:07:35 +0530

    add a+b code C2.c

--- c/C2.c
+++ c/C2.c
@@ -1,1 +1,11 @@
-h
+#include <stdio.h>
+int main() {
+int a, b;
+printf("Write value of A and press enter");
+scanf("%d",&a)
+printf("Write value of B and press enter");
+scanf("%d",&b)
+
+printf("%d\n",a+b);
+return 0;
+}
