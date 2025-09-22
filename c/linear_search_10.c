e3a6424f38b42040f6675670006529971bb45258
AYUSH <94125343+AYUSH-71643@users.noreply.github.com>
2025-09-23 00:55:41 +0530

    Create linear_search_10.c

---
+++ c/linear_search_10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {20, 100, 45, 90, 102};
+    int search = 90;
+    int i;
+    for (i = 0; i < 5; i++) {
+        if (arr[i] == search) {
+            printf("Element is found at index: %d
+", i);
+            break;
+        }
+    }
+    if (i == 5) {
+        printf("Element not found
+");
+    }
+    return 0;
+}
