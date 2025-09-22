51641afd0f697e80e6ea6ff3fd4ae608f5b537e0
AYUSH <94125343+AYUSH-71643@users.noreply.github.com>
2025-09-23 01:01:38 +0530

    Create area_rectangle_16.c

---
+++ c/area_rectangle_16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("Length = ");
+    scanf("%d", &a);
+    int b;
+    printf("Width = ");
+    scanf("%d", &b);
+    printf("Area of the Rectangle is %d
+", a * b);
+    return 0;
+}
