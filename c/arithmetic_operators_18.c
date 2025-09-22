91badb84ae028007d9666dc9421cacacbea7e165
AYUSH <94125343+AYUSH-71643@users.noreply.github.com>
2025-09-23 01:03:31 +0530

    Create arithmetic_operators_18.c

---
+++ c/arithmetic_operators_18.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a = 2;
+    int b = 4;
+    int c = 6;
+
+    printf("a+b=%d
+", a + b);
+    printf("b-c=%d
+", b - c);
+    printf("c*b=%d
+", c * b);
+    printf("a/b=%d
+", a / b);
+    return 0;
+}
