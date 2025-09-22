d877f27a1f656981dca2d1a066bc0e960c00d8b3
AYUSH <94125343+AYUSH-71643@users.noreply.github.com>
2025-09-23 01:02:17 +0530

    Create relational_operators_17.c

---
+++ c/relational_operators_17.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() {
+    int a = 2;
+    int b = 4;
+    int c = 6;
+    int d = 5;
+
+    if (a == c) {
+        printf("True
+");
+    } else {
+        printf("False
+");
+    }
+    if (a <= c) {
+        printf("True
+");
+    } else {
+        printf("False
+");
+    }
+    if (a < b) {
+        printf("True
+");
+    } else {
+        printf("False
+");
+    }
+    if (a > b) {
+        printf("True
+");
+    } else {
+        printf("False
+");
+    }
+    if (b >= c) {
+        printf("True
+");
+    } else {
+        printf("False
+");
+    }
+    if (b <= c) {
+        printf("True
+");
+    } else {
+        printf("False
+");
+    }
+    return 0;
+}
